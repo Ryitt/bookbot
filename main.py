89b89986f819b26f60e2d39f23f80cfdbd0989f9
Jukka Ruotsalainen <jukka.ruots@gmail.com>
2025-01-07 19:01:12 +0200

    Bookbot Done

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+print("hello world")
+
 def main():
     print(get_book_content("books/frankenstein.txt"))
     print(count_words("books/frankenstein.txt"))
     print(count_chars("books/frankenstein.txt"))
+    print_chars_stats("books/frankenstein.txt")
 
 def get_book_content(bookfile):
     with open(bookfile) as f:
@@ -22,6 +25,14 @@ def count_chars(bookfile):
         else:
             char_count[char] = 1
     return char_count
+
+def print_chars_stats(bookfile):
+    chars = count_chars(bookfile)
+    sum_chars = sum(chars.values())
+    print(f"{sum_chars} words found in the document")
+    for key, value in chars.items():
+        print(f"The '{key}' character was found {value} times")
+
 
 
 main()
